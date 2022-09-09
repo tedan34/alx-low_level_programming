607c87b0eef38395403d85e0028c94577f873448
tedan34 <otienotedd17@gmail.com>
2022-09-09 10:33:59 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
 
 * main - print the string in the put function
@@ -17,7 +15,6 @@
 */
 
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
