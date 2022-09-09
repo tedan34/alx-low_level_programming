0c195cc4b891eb3c8caa29488847a2aaaa1148f9
tedan34 <otienotedd17@gmail.com>
2022-09-09 15:43:46 -0700

    coding

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints and that piece of art is useful",
+* - Dora Korpar, 2015-10-19,
+* Return: 1 if exited properly, zero otherwise
+*/
+int main(void)
+{
+	printf('and that piece of art is useful" - Dora Korpar, 2015-10-19');
+	return (1);
+}
